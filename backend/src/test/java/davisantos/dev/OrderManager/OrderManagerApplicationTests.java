b946ef29f199d5aacf86cf2006ea3df291318857
Davi Santos <davicoutinho984@gmail.com>
2026-04-22 15:52:09 -0300

    chore: setup backend and frontend structure

---
+++ backend/src/test/java/davisantos/dev/OrderManager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package davisantos.dev.OrderManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
