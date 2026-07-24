5d0148b21e4ca017a4554cee45c150e016288a9c
Davi Santos <davicoutinho984@gmail.com>
2026-07-24 16:12:19 -0300

    feat: add map-struct to pom.xml

--- backend/src/main/java/davisantos/dev/OrderManager/modules/order/dto/OrderItemResponse.java
+++ backend/src/main/java/davisantos/dev/OrderManager/modules/order/dto/OrderItemResponse.java
@@ -1,5 +1,7 @@
 package davisantos.dev.OrderManager.modules.order.dto;
 
+import davisantos.dev.OrderManager.modules.product.dto.ProductResponse;
+
 import java.math.BigDecimal;
 
 public record OrderItemResponse(
--- backend/src/main/java/davisantos/dev/OrderManager/modules/order/dto/OrderResponse.java
+++ backend/src/main/java/davisantos/dev/OrderManager/modules/order/dto/OrderResponse.java
@@ -1,9 +1,11 @@
 package davisantos.dev.OrderManager.modules.order.dto;
 
+import java.util.List;
+
 public record OrderResponse(
         Long id,
         String client,
-        list<OrderItemResponse> orderItems,
+        List<OrderItemResponse> orderItems,
         String status
 ) {
 }
