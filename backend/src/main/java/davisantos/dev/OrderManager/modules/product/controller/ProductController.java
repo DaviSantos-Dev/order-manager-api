cbe4b6d04c8ab35554b79cd0003feef759c6b66e
Davi Santos <davicoutinho984@gmail.com>
2026-07-27 16:07:19 -0300

    feat: improve product controller

--- backend/src/main/java/davisantos/dev/OrderManager/modules/product/controller/ProductController.java
+++ backend/src/main/java/davisantos/dev/OrderManager/modules/product/controller/ProductController.java
@@ -2,50 +2,46 @@ package davisantos.dev.OrderManager.modules.product.controller;
 
 import davisantos.dev.OrderManager.modules.product.dto.*;
 import davisantos.dev.OrderManager.modules.product.service.ProductService;
+import davisantos.dev.OrderManager.shared.utils.GenericController;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-public class ProductController {
+public class ProductController implements GenericController {
 
-    private ProductService service;
+    private ProductService productService;
 
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest dto) {
-        ProductResponse product = service.create(dto);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.id()).toUri();
-
-        return ResponseEntity.created(uri).body(product);
+        ProductResponse product = productService.create(dto);
+        return ResponseEntity.created(generateUri(product.id())).body(product);
     }
 
     @GetMapping
     public ResponseEntity<List<ProductResponse>> findAll() {
-        return ResponseEntity.ok(service.findAll());
+        return ResponseEntity.ok(productService.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.findById(id));
+        return ResponseEntity.ok(productService.findById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id,@Valid @RequestBody ProductRequest dto) {
-        ProductResponse response = service.update(id, dto);
+        ProductResponse response = productService.update(id, dto);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        service.softDeleteById(id);
+        productService.softDeleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
